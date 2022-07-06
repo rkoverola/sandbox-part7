7fd562bf5fb53968f329566746e4e2a44f212391
Risto Koverola <risto.koverola@eficode.com>
2022-07-06 17:33:55 +0300

    Add styled components

--- sandbox/src/App.js
+++ sandbox/src/App.js
@@ -8,7 +8,6 @@ import {
   TableRow,
   Paper,
   TextField,
-  Button,
   Alert,
   AppBar,
   Toolbar
@@ -24,6 +23,37 @@ import {
   useMatch,
 } from "react-router-dom"
 
+
+import styled from 'styled-components'
+
+const Button = styled.button`
+  background: Bisque;
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid Chocolate;
+  border-radius: 3px;
+`
+
+const Input = styled.input`
+  margin: 0.25em
+`
+
+const Page = styled.div`
+  padding: 1em;
+  background: papayawhip;
+`
+
+const Navigation = styled.div`
+  background: BurlyWood;
+  padding: 1em;
+`
+
+const Footer = styled.div`
+  background: Chocolate;
+  padding: 1em;
+  margin-top: 1em;
+`
 
 const Home = () => (
   <div>
@@ -147,43 +177,29 @@ const App = () => {
   }
 
   return (
-    <Container>
-      {(message &&
-        <Alert severity="success">
-          {message}
-        </Alert>
-      )}
-      <AppBar position="static">
-        <Toolbar>
-          <Button color="inherit" component={Link} to="/">
-            home
-          </Button>
-          <Button color="inherit" component={Link} to="/notes">
-            notes
-          </Button>
-          <Button color="inherit" component={Link} to="/users">
-            users
-          </Button>   
-          {user
-            ? <em>{user} logged in</em>
-            : <Button color="inherit" component={Link} to="/login">
-                login
-              </Button>
-          }                              
-        </Toolbar>
-      </AppBar>
+    <Page>
+      <Navigation>
+        <Link style={padding} to="/">home</Link>
+        <Link style={padding} to="/notes">notes</Link>
+        <Link style={padding} to="/users">users</Link>
+        {user
+          ? <em>{user} logged in</em>
+          : <Link style={padding} to="/login">login</Link>
+        }
+      </Navigation>
+      
       <Routes>
-        <Route path="/notes/:id" element={<Note note={note} />} />
-        <Route path="/notes" element={<Notes notes={notes} />} />
+        <Route path="/notes/:id" element={<Note note={note} />} />  
+        <Route path="/notes" element={<Notes notes={notes} />} />   
         <Route path="/users" element={user ? <Users /> : <Navigate replace to="/login" />} />
         <Route path="/login" element={<Login onLogin={login} />} />
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home />} />      
       </Routes>
-      <div>
-        <br />
+
+      <Footer>
         <em>Note app, Department of Computer Science 2022</em>
-      </div>
-    </Container>
+      </Footer>
+    </Page>
   )
 }
 
