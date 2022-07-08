68758a4b730fb91d15359135b11b24dd0ee98fff
Risto Koverola <risto.koverola@eficode.com>
2022-07-08 11:14:42 +0300

    Change singlequote and semicolon ruke

--- blog-frontend/.eslintrc.js
+++ blog-frontend/.eslintrc.js
@@ -1,56 +1,44 @@
 /* eslint-env node */
 module.exports = {
-  "env": {
-      "browser": true,
-      "es6": true,
-      "jest/globals": true,
-      "cypress/globals": true
+  env: {
+    browser: true,
+    es6: true,
+    "jest/globals": true,
+    "cypress/globals": true,
   },
-  "extends": [ 
-      "eslint:recommended",
-      "plugin:react/recommended",
-      "prettier",
-      "plugin:prettier/recommended"
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "prettier",
+    "plugin:prettier/recommended",
   ],
-  "parserOptions": {
-      "ecmaFeatures": {
-          "jsx": true
-      },
-      "ecmaVersion": 2018,
-      "sourceType": "module"
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: "module",
   },
-  "plugins": [
-      "react", "jest", "cypress", "prettier"
-  ],
-  "rules": {
-      "indent": [
-          "error",
-          2  
-      ],
-      "linebreak-style": ['error', process.platform === 'win32' ? 'windows' : 'unix'],
-      "quotes": [
-          "error",
-          "single"
-      ],
-      "semi": [
-          "error",
-          "never"
-      ],
-      "eqeqeq": "error",
-      "no-trailing-spaces": "error",
-      "object-curly-spacing": [
-          "error", "always"
-      ],
-      "arrow-spacing": [
-          "error", { "before": true, "after": true }
-      ],
-      "no-console": 0,
-      "react/prop-types": 0,
-      "react/react-in-jsx-scope": "off"
+  plugins: ["react", "jest", "cypress", "prettier"],
+  rules: {
+    indent: ["error", 2],
+    "linebreak-style": [
+      "error",
+      process.platform === "win32" ? "windows" : "unix",
+    ],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+    eqeqeq: "error",
+    "no-trailing-spaces": "error",
+    "object-curly-spacing": ["error", "always"],
+    "arrow-spacing": ["error", { before: true, after: true }],
+    "no-console": 0,
+    "react/prop-types": 0,
+    "react/react-in-jsx-scope": "off",
   },
-  "settings": {
-    "react": {
-      "version": "detect"
-    }
-  }
-}
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
